495bb965c1cf576ad7e1be898c94f5408c4a55b3
Q Corbin <dquindara8@gmail.com>
2024-05-06 17:14:36 -0400

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 // Footer.js
 import React from 'react';
+import './Footer.css'; // Import the CSS file for styling
 
 const Footer = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header'; // Import your Header component
+import Footer from './components/Footer'; // Import the Footer component
 import './index.css'; // Import your CSS file
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     {/* Other components will go here */}
+    <Footer /> {/* Include the Footer component */}
   </React.StrictMode>,
   document.getElementById('root')
 );
